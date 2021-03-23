8bfd9c2fb0f94aa074d5adceb7f2a3d3e231a34b
prabhkirank12 <prabhkirank2@gmail.com>
2021-03-22 22:44:02 -0400

    TODO App

---
+++ todo_app/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <input />
+      <button>Add Todo</button>
+
+      <ul>
+        <li>Make a project</li>
+        <li>Take a walk</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
